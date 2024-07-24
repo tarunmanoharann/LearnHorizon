a72787a748a5ade3388d342b6fd25eb2470dee5c
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-07-24 10:02:00 +0530

    setup initialisation
     (tsx + tailwind) completed

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+// App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Academics from './pages/Academics';
+import Admissions from './pages/Admissions';
+import Faculty from './pages/Faculty';
+import Students from './pages/Students';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen bg-white">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/academics" element={<Academics />} />
+            <Route path="/admissions" element={<Admissions />} />
+            <Route path="/faculty" element={<Faculty />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
